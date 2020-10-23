0ba91bd1ff81a1b5069f7a36f305d3c8d2f5c178
Elena Vonder Meulen <evonder13@gmail.com>
2020-10-23 12:10:27 -0400

    exception and collection added

---
+++ AdvancedCSharpProject/BootcampException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedCSharpProject
+{
+    class BootcampException : Exception
+    {
+        public BootcampException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+
+        public BootcampException(string message) : base(message)
+        {
+
+        }
+
+        public BootcampException() : base()
+        {
+
+        }
+    }
+}
--- AdvancedCSharpProject/Program.cs
+++ AdvancedCSharpProject/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdvancedCSharpProject
 {
     class Program
     {
+        //Rethrowing Exception
+        private void RethrowingExceptions()
+        {
+            try
+            {
+                throw new BootcampException("Throw this exception");
+                //var ex = new BootcampException ("Throw this exception")
+                //throw ex
+            }
+            catch (BootcampException ex)
+            {
+                //if we cannot handle the exception
+                //just stating throw; cancels catch block
+                throw ex;
+            }
+        }
+        //Collection initializer
+        //List <int>
+        List<int> ints = new List<int> { 1, 2, 3, 4, 5 };
+
+        //dictionary<int, bootcamp>
+        Dictionary<string, Bootcamp> bootcamps = new Dictionary<string, Bootcamp>
+        {
+            ["CSharp"] = new Bootcamp(),
+            ["Java"] = new Bootcamp(),
+        };
+        void AccessDictionary()
+        {
+            var csharpBc = bootcamps["CSharp"];
+        }
+
         static int i = 10;
         static string a = "ABC";
 
         //static means you don't have make an instance of the variable or even class. Just call it in STATIC MAIN METHOD
         static void Main(string[] args)
         {
+
+
             //out patameter
             var nbrStr = "123";//with a character like "f" it would come back false because f cannot be an integer
             int nbr; //if you put 'var' in front of 'nbr' you have to give it a value
